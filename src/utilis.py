2e9a0f1f0ec06d57d9c99016ab9f8a83ca88eafe
Dhruv Bansal <dbansal1_be23@thapar.edu>
2026-01-19 18:17:15 +0530

    model trainer

--- src/utilis.py
+++ src/utilis.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+from sklearn.metrics import r2_score 
 
 import numpy as np
 import pandas as pd
@@ -17,4 +18,28 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys) 
- 
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predict on test data
+            y_pred = model.predict(X_test)
+
+            # Calculate R2 score
+            r2_square = r2_score(y_test, y_pred)
+
+            report[model_name] = r2_square
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)   
+  
